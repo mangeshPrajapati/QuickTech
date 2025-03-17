2900447a1ed61442ed2598acb50857ec180b0739
mangeshPrajapati <mangeshprajapati297@gmail.com>
2025-03-17 14:07:58 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,6 @@ export default defineConfig({
   root: path.resolve(__dirname, "client"),
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
-    // emptyOutDir: true,
+    emptyOutDir: true,
   },
 });
